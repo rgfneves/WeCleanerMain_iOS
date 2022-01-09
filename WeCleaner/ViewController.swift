20ec28819a71ad71798e3e7b1a60a4251852a978
Rafael Neves <rgfneves@gmail.com>
2022-01-09 14:44:26 -0300

    Initial Commit

---
+++ WeCleaner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeCleaner
+//
+//  Created by Rafael Gomes Ferreira Neves on 09/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
